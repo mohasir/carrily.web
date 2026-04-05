426dc6ba7c65e755c28aad76194542ce960e9641
SamBarFu <samuelbarberena12@gmail.com>
2026-04-05 01:21:42 -0600

    landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefault: false
+    }
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
